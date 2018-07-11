15d74b76ee59d2a7dd9ec4da8b42f24d4da43074
Samuel J Mathew <samuelj90@gmail.com>
2018-07-11 19:01:14 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# spring-boot-micro-service-seed
+# Spring Boot Micro Service Seed
+
+This is a spring boot microservices seed project by combaining widely adopted technologies into a fully functional application base.
---
+++ boot-admin-server/src/main/java/com/mycrazycodes/springboot/bootadminserver/BootAdminServerApplication.java
@@ -0,0 +1,12 @@
+package com.mycrazycodes.springboot.bootadminserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootAdminServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootAdminServerApplication.class, args);
+	}
+}
