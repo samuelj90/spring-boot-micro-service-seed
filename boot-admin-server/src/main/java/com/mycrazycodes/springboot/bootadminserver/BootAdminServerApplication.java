1d8ac4a9ea19596e2add95b3cde55830f3eb7bb5
Samuel J Mathew <samuelj90@gmail.com>
2018-07-25 18:20:35 +0530

    Initial commit

--- boot-admin-server/src/main/java/com/mycrazycodes/springboot/bootadminserver/BootAdminServerApplication.java
+++ boot-admin-server/src/main/java/com/mycrazycodes/springboot/bootadminserver/BootAdminServerApplication.java
@@ -1,9 +1,11 @@
 package com.mycrazycodes.springboot.bootadminserver;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAdminServer
 public class BootAdminServerApplication {
 
 	public static void main(String[] args) {
